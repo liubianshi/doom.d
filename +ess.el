dcec8b6079a1039a2bc7c9e3613b072bd1e7745c
liu.bian.shi <liu.bian.shi@gmail.com>
2021-07-20 07:33:59 +0800

    litter update

--- +ess.el
+++ +ess.el
@@ -2,15 +2,15 @@
 (use-package! ess
   :config
   (setq comint-move-point-for-output t)
-(defun then_R_operator ()
-  "R - %>% operator or 'then' pipe operator"
-  (interactive)
-  (just-one-space 1)
-  (insert "%>%")
-  (reindent-then-newline-and-indent))
+  (defun then_R_operator ()
+    "R - %>% operator or 'then' pipe operator"
+    (interactive)
+    (just-one-space 1)
+    (insert "%>%")
+    (reindent-then-newline-and-indent))
 
-(map! :map ess-r-mode-map
-      :i "M-=" #'ess-insert-assign
-      :i "M-\\" #'then_R_operator)
+  (map! :map ess-r-mode-map
+        :i "M-=" #'ess-insert-assign
+        :i "M-\\" #'then_R_operator)
   )
 

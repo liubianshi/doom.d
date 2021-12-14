fdd955283d01dc478f45995c7d9c26fc1f094d67
liu.bian.shi <liu.bian.shi@gmail.com>
2021-12-14 10:46:14 +0800

    some config from mac

--- +basic.el
+++ +basic.el
@@ -2,10 +2,10 @@
 (if (eq system-type 'darwin)
     (setq fontsize 16
           font-monospace "FiraCode Nerd Font Mono"
-          font-monospace-sc "Sarasa Mono Slab SC"
-          font-sans "PingFang SC"
+          font-monospace-sc "Source Han Serif SC"
+          font-sans "Source Han Serif SC"
           font-serif "Noto Serif SC"
-          font-weight 'Regular
+          font-weight 'regular
           line-space 8
           doom-theme 'doom-acario-light
           )
@@ -14,11 +14,10 @@
         font-monospace-sc "Sarasa Mono Slab SC"
         font-sans "Noto Sans CJK SC"
         font-serif "Noto Serif CJK SC"
-        font-weight 'Regular
+        font-weight 'regular
         line-space 8
         doom-theme 'doom-acario-dark
         ))
-
 
 ;;; 启动管理
 (setq window-system-default-frame-alist
@@ -36,8 +35,8 @@
         (nil
          (menu-bar-lines . 0)
          (tool-bar-lines . 0)
-         ;; (background-color . "black")
-         ;; (foreground-color . "white")
+         (background-color . "black")
+         (foreground-color . "white")
          )))
 
 ;; Window Title
@@ -223,4 +222,4 @@ Also immediately enables `mixed-pitch-modes' if currently in one of the modes."
                 ))))
 
 
-(setq +zen-text-scale 0.5)
+(setq +zen-text-scale 0)

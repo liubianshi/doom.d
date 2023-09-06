06a18bd1a52219339e2af63e932425b755b051a2
liu.bian.shi <liu.bian.shi@gmail.com>
2023-09-06 18:21:03 +0800

    citar update

--- +citar.el
+++ +citar.el
@@ -1,12 +1,12 @@
-(use-package citar
+(use-package! citar
   :custom
-  (citar-bibliography '("~/Documents/paper_ref.bib"))
+  (citar-bibliography '("~/Documents/paper_ref.bib" "~/Documents/url_ref.bib"))
   :hook
   (LaTeX-mode . citar-capf-setup)
   (markdown-mode . citar-capf-setup)
   (org-mode . citar-capf-setup))
 
-(use-package citar-embark
+(use-package! citar-embark
   :after citar embark
   :no-require
   :config (citar-embark-mode))
